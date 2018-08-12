adc35f7e56d479815794b3d8beb05b0fc989b1b5
Evans Owino <evansowino@hotmail.com>
2018-08-12 15:57:53 +0100

    providers

---
+++ Providers.ps1
@@ -0,0 +1,10 @@
+# List default Providers
+Get-PSProvider
+
+# See environment variables
+Set-Location env:
+Get-ChildItem
+
+# See aliases in current PowerShell env
+Set-Location alias:
+Get-ChildItem
