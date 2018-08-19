4a05e721e5e04687cfdcd5d2b3c2ae491eec4a56
Evans Owino <evansowino@hotmail.com>
2018-08-19 16:51:24 +0100

    loops

--- branch_loop_scope.ps1
+++ branch_loop_scope.ps1
@@ -52,3 +52,57 @@ switch -Wildcard ("Switch") {
     "SWITC?" { "?" }
     Default { "No idea" }
 }
+
+# While loop
+# won't execute if condition is true
+$a = 4
+while ($a -le 5) {
+    "$a"
+    $a = $a + 1
+}
+
+# Do while loop
+do {
+    $a
+    $a++
+} while ($a -le 5)
+
+# Do until - used to make checking positive
+do {
+    $a
+    $a++
+} until ($a -le 5)
+
+
+# For loop
+for ($f = 0; $f -le 5; $f++) {
+    "`$f = $f"
+}
+
+# Iteration over collections
+$arr = 3, 32, 4, 5
+for ($i = 0; $i -lt $arr.Length; $i++) {
+    "`$array[$i]=" + $array[$i]
+}
+
+# foreach woks on collections
+foreach ($item in $arr) {
+    "`$item = $item"
+}
+
+foreach ($file in Get-ChildItem) {
+    if ($file.Name -like "*.ps1") {
+        $file.Name
+        # use break to exit out foreach loop
+        # use continue to move onto next iteration
+    }
+}
+
+# Use loop labels to break to a certain loop
+:itemloop foreach ($item in 1..4) {
+    "`$item = $item"
+    foreach ($otherItem in 4..7) {
+        "`$otherItem = $otherItem"
+        break itemloop
+    }
+}
